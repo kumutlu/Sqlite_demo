f91f969cb4aa1c4debe632408af16229f080ad43
Kemal Oguz Umutlu <kumutlu@gmail.com>
2019-01-27 12:33:59 +0300

    Add files via upload

---
+++ car_list.py
@@ -0,0 +1,11 @@
+from cars import Cars
+
+
+car_1 = Cars("Honda", "Civic", 2016, 85000)
+car_2 = Cars("Renault", "Fluance", 2015, 80000)
+car_3 = Cars("Hyundai", "i20", 2009, 35000)
+car_4 = Cars("Volkswagen", "Polo", 2016, 75000)
+car_5 = Cars("Honda", "CRV", 2016, 85000)
+car_6 = Cars("Renault", "Megane", 2015, 80000)
+car_7 = Cars("Hyundai", "i30", 2009, 35000)
+car_8 = Cars("Volkswagen", "Golf", 2016, 75000)
